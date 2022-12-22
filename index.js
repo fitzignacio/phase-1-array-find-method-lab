9baa43d66ba84aa229e4d3449c327899bc32d1d8
Fitz Ignacio <ignacio.fitz@gmail.com>
2022-12-22 14:09:41 +0530

    commit tree 0353ebe146881b258f2e356fac112bb0c3b6cfaa

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]   
+ 
+function superbowlWin(array) {
+    for (const record of array) {
+        if (record.result === "W") {
+            return record.year;        }
+}}
+console.log(record.find(superbowlWin));
